2fda07e476be3080b3afbecb7f4e50abdcaf8140
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-02-08 10:10:34 -0500

    Modified email time and title.

--- apps/student_data/management/commands/send_mail.py
+++ apps/student_data/management/commands/send_mail.py
@@ -1,30 +1,25 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.core.management.base import BaseCommand
 from django.template.loader import get_template
 from post_office import mail
-from .models import StudentData
 import datetime
 import pytz
 
 
-@receiver(post_save, sender=StudentData)
-def send_welcome_email(sender, instance, created, **kwargs):
-
-    if created:
-
+class Command(BaseCommand):
+    def handle(self, *args, **options):
         current_time = datetime.datetime.now()
 
         current_time = current_time.astimezone(
             pytz.timezone('US/Eastern')).strftime('%d %B %Y %I:%M %p')
 
         context = {
-            'first_name': instance.first_name,
-            'last_name': instance.last_name,
+            'first_name': 'app.mrashid',
+            'last_name': '.net',
             'date_time': current_time,
         }
 
         template = get_template(
             'student_data/welcome-email.html').render(context)
 
-        mail.send([instance.email], "Mamoon Rashid <no-reply@mrashid.net>", subject="Welcome to app.mrashid.net",
+        mail.send(['hello@mrashid.net'], "Mamoon Rashid <no-reply@mrashid.net>", subject="Welcome to app.mrashid.net",
                   html_message=template, priority='now')
