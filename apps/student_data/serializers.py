9e59f8e22b2875959b91e9f97b4a652ee4639620
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-09-03 04:18:16 -0400

    Added history to student serializer.

--- apps/student_data/serializers.py
+++ apps/student_data/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-from .models import StudentData, StudentProgress
+from apps.student_data.models import StudentData, StudentProgress
+
+
+class HistoricalRecordField(serializers.ListField):
+    child = serializers.DictField()
+
+    def to_representation(self, data):
+        return super().to_representation(data.values())
 
 
 class StudentProgressSerializer(serializers.ModelSerializer):
@@ -19,10 +26,18 @@ class StudentDataListSerializer(serializers.ModelSerializer):
 
     username = serializers.CharField(source="user.username")
 
+    history = serializers.SerializerMethodField()
+
+    def get_history(self, obj):
+        # using slicing to exclude current field values
+        model_history = obj.history.filter(id=obj.pk).values(
+            'history_date', 'history_user_id').order_by('-history_date').first()
+        return model_history
+
     class Meta:
         model = StudentData
         fields = ('id', 'email', 'username', 'first_name', 'last_name', 'destination',
-                  'degree', 'major', 'english_proficiency', 'created', 'status', 'rating')
+                  'degree', 'major', 'english_proficiency', 'created', 'status', 'rating', 'history')
 
 
 class StudentDataBriefSerializer(serializers.ModelSerializer):
